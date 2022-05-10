ab421ce8e4e41cdcdbbd29d736291c3837288068
Pablo Buisson <pablo.buisson@gmail.com>
2022-05-10 07:29:45 +0200

    create a reusable widget for the FutureProvider examples

--- lib/screens/advices_screen.dart
+++ lib/screens/advices_screen.dart
@@ -49,17 +49,7 @@ class AdvicesScreen extends ConsumerWidget {
               textAlign: TextAlign.center,
               textScaleFactor: 1.4,
             ),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: Text(
-                adviceRandom.when(
-                    data: (Advice value) => value.advice,
-                    error: (Object error, _) =>
-                        "A problem occurred, please try again later",
-                    loading: () => "Loading..."),
-                textAlign: TextAlign.center,
-              ),
-            ),
+            AdviceBlock(advice: adviceRandom),
             const SizedBox(
               height: 50,
             ),
@@ -68,19 +58,36 @@ class AdvicesScreen extends ConsumerWidget {
               textAlign: TextAlign.center,
               textScaleFactor: 1.4,
             ),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: Text(
-                adviceRandomId.when(
-                    data: (Advice value) => value.advice,
-                    error: (Object error, _) =>
-                        "A problem occurred, please try again later",
-                    loading: () => "Loading..."),
-                textAlign: TextAlign.center,
-              ),
-            ),
+            AdviceBlock(advice: adviceRandomId),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class AdviceBlock extends StatelessWidget {
+  const AdviceBlock({Key? key, required this.advice}) : super(key: key);
+
+  final AsyncValue<Advice> advice;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      decoration: BoxDecoration(
+          color: Colors.grey[200],
+          border:
+              const Border(left: BorderSide(color: Colors.grey, width: 5.0))),
+      child: Text(
+        advice.when(
+            data: (Advice value) => value.advice,
+            error: (Object error, _) =>
+                "A problem occurred, please try again later",
+            loading: () => "Loading..."),
+        textAlign: TextAlign.center,
+        style: const TextStyle(fontStyle: FontStyle.italic),
       ),
     );
   }
