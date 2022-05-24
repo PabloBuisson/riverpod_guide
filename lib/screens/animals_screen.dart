52c7a95d176050f914b811b98019d646d4bfa903
Pablo Buisson <pablo.buisson@gmail.com>
2022-05-24 08:13:12 +0200

    add the UI of the StateNotiferProvider example

--- lib/screens/animals_screen.dart
+++ lib/screens/animals_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../main.dart';
+import '../models/animal.dart';
 
 class AnimalsScreen extends ConsumerStatefulWidget {
   @override
@@ -17,10 +18,53 @@ class _AnimalsScreenState extends ConsumerState<AnimalsScreen> {
 
   @override
   Widget build(BuildContext context) {
+    List<Animal> animals = ref.watch(animalsProvider);
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: const Text("Favorite animals"),
+      ),
+      body: Column(
+        children: [
+          const Text("Zoo animals"),
+          animals.isNotEmpty
+              ? SizedBox(
+                  height: 200.0,
+                  child: ListView(
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      for (final animal in animals)
+                        InkWell(
+                          onTap: () => ref
+                              .read(animalsProvider.notifier)
+                              .markAsFavorite(animal.id!),
+                          child: Container(
+                              padding: const EdgeInsets.all(10.0),
+                              width: 200.0, //TODO: use MediaQuery
+                              child: Card(
+                                elevation: 10.0,
+                                child: Column(
+                                  children: [
+                                    Text(animal.name ?? "Unknown"),
+                                    Text(animal.latinName ?? ""),
+                                    Icon(
+                                      Icons.favorite,
+                                      color: animal.favorite
+                                          ? Colors.red
+                                          : Colors.grey,
+                                    )
+                                  ],
+                                ),
+                              )),
+                        ),
+                    ],
+                  ),
+                )
+              : Container(),
+          const Text("My favorite animals"),
+        ],
       ),
     );
   }
